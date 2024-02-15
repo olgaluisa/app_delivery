0e72c1b062720ecbaeb2faeac4b1cda006aec00c
olgaluisa <luiisaolga11@gmail.com>
2024-02-15 00:41:12 -0300

    fix: adjustments to make the code more consistent

--- src/app/product/[id].tsx
+++ src/app/product/[id].tsx
@@ -1,6 +1,7 @@
 import { Image, Text, View } from 'react-native';
 import { useLocalSearchParams, useNavigation } from 'expo-router';
 import { Feather } from '@expo/vector-icons';
+import { Redirect } from 'expo-router';
 
 import { useCartStore } from '@/stores/cart-store';
 
@@ -13,11 +14,17 @@ export default function Product() {
   const cartStore = useCartStore();
   const navegation = useNavigation();
   const { id } = useLocalSearchParams();
-  const product = PRODUCTS.filter((item) => item.id === id)[0];
+  const product = PRODUCTS.find((item) => item.id === id);
 
   function handleAddToCart() {
-    cartStore.add(product);
-    navegation.goBack();
+    if (product) {
+      cartStore.add(product);
+      navegation.goBack();
+    }
+  }
+
+  if (!product) {
+    return <Redirect href={'/'} />;
   }
 
   return (
@@ -28,6 +35,7 @@ export default function Product() {
         resizeMode="cover"
       />
       <View className="p-5 mt-8 flex-1">
+        <Text className="text-white text-xl font-heading">{product.title}</Text>
         <Text className="text-lime-400 text-2xl font-heading my-2">
           {FormatCurrency(product.price)}
         </Text>
