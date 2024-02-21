21781c947ba6378d10b9bb6384039bb8932fdcd6
olgaluisa <luiisaolga11@gmail.com>
2024-02-20 22:09:08 -0300

    feat: Created input component for sending address by user and function to remove product from cart.

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -1,11 +1,15 @@
-import { useCartStore } from '@/stores/cart-store';
+import { Text, View, ScrollView, Alert } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 
+import { ProductCartProps, useCartStore } from '@/stores/cart-store';
 import { FormatCurrency } from '@/utils/functions/format-currency';
 
 import { Header } from '@/components/header';
 import { Product } from '@/components/product';
-
-import { Text, View, ScrollView } from 'react-native';
+import { Input } from '@/components/input';
+import { Button } from '@/components/button';
+import { LinkButton } from '@/components/link-button';
 
 export default function Cart() {
   const cartStore = useCartStore();
@@ -17,26 +21,58 @@ export default function Cart() {
     ),
   );
 
+  function handleProductRemove(product: ProductCartProps) {
+    Alert.alert('Remover', `Deseja remover ${product.title} do carrinho ?`, [
+      {
+        text: 'Cancelar',
+      },
+      {
+        text: 'Remover',
+        onPress: () => cartStore.remove(product.id),
+      },
+    ]);
+  }
+
   return (
     <View className="flex-1 pt-8">
       <Header title="Seu carrinho" />
-      <ScrollView>
-        {cartStore.products.length > 0 ? (
+      <KeyboardAwareScrollView>
+        <ScrollView>
           <View className="p-5 flex-1">
-            {cartStore.products.map((product) => (
-              <Product key={product.id} data={product} />
-            ))}
+            {cartStore.products.length > 0 ? (
+              <View className="border-b border-slate-700">
+                {cartStore.products.map((product) => (
+                  <Product
+                    key={product.id}
+                    data={product}
+                    onPress={() => handleProductRemove(product)}
+                  />
+                ))}
+              </View>
+            ) : (
+              <Text className="font-body text-slate-400 text-center my-8">
+                Seu carrinho está vazio.
+              </Text>
+            )}
+            <View className="flex-row gap-2 items-center mt-5 mb-4">
+              <Text className="text-white text-xl font-subtitle">Total: </Text>
+              <Text className="text-lime-400 text-2xl font-heading">
+                {total}
+              </Text>
+            </View>
+            <Input placeholder="Informe o endereço de entrega com rua, bairro, CEP, número e complemento..." />
           </View>
-        ) : (
-          <Text className="font-body text-slate-400 text-center my-8">
-            Seu carrinho está vazio.
-          </Text>
-        )}
-        <View className="flex-row gap-2 items-center mt-5 mb-4">
-          <Text className="text-white text-xl font-subtitle">Total: </Text>
-          <Text className="text-lime-400 text-2xl font-heading">{total}</Text>
-        </View>
-      </ScrollView>
+        </ScrollView>
+      </KeyboardAwareScrollView>
+      <View className="p-5 gap-5">
+        <Button>
+          <Button.Text>Enviar pedido</Button.Text>
+          <Button.Icon>
+            <Feather name="arrow-right-circle" size={20} />
+          </Button.Icon>
+        </Button>
+        <LinkButton title="Voltar ao cardápio" href="/" />
+      </View>
     </View>
   );
 }
--- src/stores/cart-store.ts
+++ src/stores/cart-store.ts
@@ -8,6 +8,7 @@ export type ProductCartProps = ProductProps & {
 type StateProps = {
   products: ProductCartProps[];
   add: (product: ProductProps) => void;
+  remove: (productId: string) => void;
 };
 
 export const useCartStore = create<StateProps>((set) => ({
@@ -15,5 +16,9 @@ export const useCartStore = create<StateProps>((set) => ({
   add: (product: ProductProps) =>
     set((state) => ({
       products: cartInMemory.add(state.products, product),
+    })),
+  remove: (productId: string) =>
+    set((state) => ({
+      products: cartInMemory.remove(state.products, productId),
     })),
 }));
