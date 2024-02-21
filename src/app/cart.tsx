aed0def67ac20d72f22062de2a1bf5964f7c1b67
olgaluisa <luiisaolga11@gmail.com>
2024-02-21 01:02:11 -0300

    feat:added function for sending orders to the establishment's WhatsApp.

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -1,4 +1,6 @@
-import { Text, View, ScrollView, Alert } from 'react-native';
+import { useState } from 'react';
+import { useNavigation } from 'expo-router';
+import { Text, View, ScrollView, Alert, Linking } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 
@@ -11,8 +13,12 @@ import { Input } from '@/components/input';
 import { Button } from '@/components/button';
 import { LinkButton } from '@/components/link-button';
 
+const PHONE_NUMBER = '5599999999999';
+
 export default function Cart() {
+  const [address, setAddress] = useState('');
   const cartStore = useCartStore();
+  const navegation = useNavigation();
 
   const total = FormatCurrency(
     cartStore.products.reduce(
@@ -31,6 +37,31 @@ export default function Cart() {
         onPress: () => cartStore.remove(product.id),
       },
     ]);
+  }
+
+  function handleOrder() {
+    if (address.trim().length === 0) {
+      return Alert.alert('Pedido', 'Informe os dados da entrega.');
+    }
+
+    const products = cartStore.products
+      .map((product) => `\n ${product.quantity}${product.title}`)
+      .join('');
+
+    const message = `
+   🍔 NOVO PEDIDO
+    \n Entregar em: ${address}
+    
+    ${products}
+
+    \n Valor total: ${total}
+    `;
+
+    Linking.openURL(
+      `http://api.whatsapp.com/send?phone=${PHONE_NUMBER}&text=${message}`,
+    );
+    cartStore.clear();
+    navegation.goBack();
   }
 
   return (
@@ -60,12 +91,18 @@ export default function Cart() {
                 {total}
               </Text>
             </View>
-            <Input placeholder="Informe o endereço de entrega com rua, bairro, CEP, número e complemento..." />
+            <Input
+              placeholder="Informe o endereço de entrega com rua, bairro, CEP, número e complemento..."
+              onChangeText={setAddress}
+              blurOnSubmit={true}
+              onSubmitEditing={handleOrder}
+              returnKeyType="next"
+            />
           </View>
         </ScrollView>
       </KeyboardAwareScrollView>
       <View className="p-5 gap-5">
-        <Button>
+        <Button onPress={handleOrder}>
           <Button.Text>Enviar pedido</Button.Text>
           <Button.Icon>
             <Feather name="arrow-right-circle" size={20} />
